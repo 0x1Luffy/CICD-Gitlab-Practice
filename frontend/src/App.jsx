06c7855a11a26718f41ffbe7548b9f29065766c2
0x1Luffy <chetankesare06@gmail.com>
2024-11-24 19:15:34 +0530

    Added Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,51 @@
+import  { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+    const [tasks, setTasks] = useState([]);
+    const [title, setTitle] = useState('');
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/tasks')
+            .then(res => setTasks(res.data))
+            .catch(err => console.error(err));
+    }, []);
+
+    const addTask = () => {
+        axios.post('http://localhost:3000/api/tasks', { title })
+            .then(res => setTasks([...tasks, res.data]))
+            .catch(err => console.error(err));
+    };
+
+    const deleteTask = (id) => {
+        axios.delete(`http://localhost:3000/api/tasks/${id}`)
+            .then(() => setTasks(tasks.filter(task => task._id !== id)))
+            .catch(err => console.error(err));
+    };
+
+    return (
+        <div className="App">
+            <h1>Task Manager</h1>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Enter task"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <button onClick={addTask}>Add Task</button>
+            </div>
+            <ul>
+                {tasks.map(task => (
+                    <li key={task._id}>
+                        {task.title}
+                        <button onClick={() => deleteTask(task._id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
